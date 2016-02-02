f17eb9d17b2dc877aafe33622c75bc159ce74722
David Nugent <davidn@uniquode.io>
2016-02-03 10:06:25 +1100

    Fix logname so we don't embed slashes into the name. Add test for logging levels.

--- tests/logging/test_basic_logging.cc
+++ tests/logging/test_basic_logging.cc
@@ -11,7 +11,7 @@ extern "C" {
 namespace {
 
     TEST(LoggingFunctions, createAndRotateLog) {
-        log_init(LOG_ECHO|LOG_SYNC, V_DEBUG, "logfile-%Y%M%D_%H%M%S.log");
+        log_init(LOG_ECHO|LOG_SYNC, V_DEBUG, "logfile-%Y%m%d_%H%M%S.log");
 
         log_trace("This should not appear in the log");
         log_debug("This is a debug entry with a %s value %d", "test", 123);
@@ -21,19 +21,22 @@ namespace {
         log_fatal("This is a fatal error message...");
         const char *log_name1 = log_name();
         EXPECT_NE((const char *)0, log_name1);
+        log_info("Rotating log! (sleeping for 1 second to ensure different name)");
         sleep(1);
         log_rotate();
+        log_info("Log is rotated!");
         const char *log_name2 = log_name();
         EXPECT_NE((const char *)0, log_name2);
         EXPECT_NE(log_name1, log_name2);
         log_warning("Log name first = '%s', second = '%s'", log_name1, log_name2);
     };
 
+    TEST(LoggingFunctions, loggingLevels) {
+        log_init(LOG_ECHO, V_TRACE, "logfile-%Y%m%d_%H%M%S.log");
+        const char *levelnames[] = { "none", "fatal", "critical", "error", "warn", "info", "debug", "trace" };
+        for (int v = V_NONE; v++ < V_MAX; ) {
+            log_message((enum Verbosity)v, "This is a log message at %s level", levelnames[v]);
+        }
+    }
 
 } // namespace
-
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
